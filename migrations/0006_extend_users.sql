b73ecf07f346c1c417cf912eaf297ac0dd158497
илья кинке <ilya@qu1nqqy.ru>
2026-07-18 17:49:37 +0900

    fix(db): DROP TRIGGER IF EXISTS в миграции 0006 для идемпотентности

--- migrations/0006_extend_users.sql
+++ migrations/0006_extend_users.sql
@@ -13,6 +13,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+DROP TRIGGER IF EXISTS trg_users_updated_at ON users;
 CREATE TRIGGER trg_users_updated_at
     BEFORE UPDATE ON users
     FOR EACH ROW EXECUTE FUNCTION set_updated_at();
